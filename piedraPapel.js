bb9cee05bb4f952ef80160dfa028e74ca0f8b92e
aroluigi <ablatman@gmail.com>
2022-05-09 21:29:39 -0300

    Cambio confirm por button

--- piedraPapel.js
+++ piedraPapel.js
@@ -7,9 +7,9 @@ var manoJugador=prompt("¿Piedra, papel o tijera? Yo ya hice mi elección.","Esc
 
 document.write("<div style=\"text-align: center; font-size:2rem\">Usted eligió \"" + manoJugador + "\" y yo elegí \"" + manoCompu + "\".<br>");
 
-if(manosPosibles.indexOf(manoJugador)==-1){document.write("Para jugar debe elegir una de las opciones.</div>")}
-else if((manosPosibles.indexOf(manoJugador)+1)%3==(manosPosibles.indexOf(manoCompu))){document.write("Usted gana.</div>")} 
-else if((manosPosibles.indexOf(manoJugador))==(manosPosibles.indexOf(manoCompu))){document.write("Empate.</div>")}
-else {document.write("Yo gano.</div>")} 
+if(manosPosibles.indexOf(manoJugador)==-1){document.write("Para jugar debe elegir una de las opciones.")}
+else if((manosPosibles.indexOf(manoJugador)+1)%3==(manosPosibles.indexOf(manoCompu))){document.write("Usted gana.")} 
+else if((manosPosibles.indexOf(manoJugador))==(manosPosibles.indexOf(manoCompu))){document.write("Empate.")}
+else {document.write("Yo gano.")} 
 
-if(confirm("¿Desea jugar de nuevo?")){location.reload()};
+document.write("<br><br><input type='button' value='Jugar de nuevo' onClick=location.reload()></div>");
