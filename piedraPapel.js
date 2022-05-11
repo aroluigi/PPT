84f7dcb38cc7b15d641f81cc743e648e4f037b44
aroluigi <ablatman@gmail.com>
2022-05-11 15:20:27 -0300

    Bucle while para jugar tres veces

--- piedraPapel.js
+++ piedraPapel.js
@@ -1,15 +1,24 @@
 
-var manosPosibles=["piedra","tijera","papel"];
+const manosPosibles=["piedra","tijera","papel"];
 
-var manoCompu=manosPosibles[Math.floor(Math.random() * 3)];
+var marcador=[0,0,0]; //[PuntosJugador, Empates, PuntosCompu]
 
-var manoJugador=prompt("¿Piedra, papel o tijera? Yo ya hice mi elección.","Escriba su elección (en minúsculas).");
+    while (marcador[0]+marcador[1]+marcador[2]<3) {
 
-document.write("<div style=\"text-align: center; font-size:2rem\">Usted eligió \"" + manoJugador + "\" y yo elegí \"" + manoCompu + "\".<br>");
+        var manoCompu=manosPosibles[Math.floor(Math.random() * 3)];
 
-if(manosPosibles.indexOf(manoJugador)==-1){document.write("Para jugar debe elegir una de las opciones.")}
-else if((manosPosibles.indexOf(manoJugador)+1)%3==(manosPosibles.indexOf(manoCompu))){document.write("Usted gana.")} 
-else if((manosPosibles.indexOf(manoJugador))==(manosPosibles.indexOf(manoCompu))){document.write("Empate.")}
-else {document.write("Yo gano.")} 
+        var manoJugador=prompt("¿Piedra, papel o tijera? Yo ya hice mi elección.","Escriba su elección (en minúsculas).");
+        
+        document.write("<div style=\"text-align: center; font-size:1.5rem;\">Usted eligió \"" + manoJugador + "\" y yo elegí \"" + manoCompu + "\".<br>");
+        
+        if(manosPosibles.indexOf(manoJugador)==-1){document.write("Para jugar debe elegir una de las opciones.")}
+        else if((manosPosibles.indexOf(manoJugador)+1)%3==(manosPosibles.indexOf(manoCompu))){
+            document.write("Usted gana."); marcador[0]++} 
+        else if((manosPosibles.indexOf(manoJugador))==(manosPosibles.indexOf(manoCompu))){
+            document.write("Empate."); marcador[1]++}
+        else {document.write("Yo gano."); marcador[2]++} 
+    }
 
-document.write("<br><br><input type='button' value='Jugar de nuevo' onClick=location.reload()></div>");
+document.write("<br><hr><p>El marcador final es:</p> Usted gana: " + marcador[0] + "<br>Empates: " + marcador[1] + "<br>Yo gano: " + marcador[2]);
+
+document.write("<hr><br><input type='button' value='Jugar de nuevo' onClick=location.reload()></div>");
